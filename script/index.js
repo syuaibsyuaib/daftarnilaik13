d643575910992c06eca666ccedfef57754c9a710
syuaibsyuaib <syuaibsyuaib@gmail.com>
2019-11-25 00:13:40 +0800

    tambahan local.isinp return false

--- script/index.js
+++ script/index.js
@@ -7,5 +7,14 @@ let local = {
     },
     hapus: function (key) {
         return localStorage.removeItem(key);
+    },
+    isiinp: function (key) {
+        for (let i = 0; i < key.length; i++) {
+            if (key[i].value == "") {
+                alert(key[i].placeholder + ' tidak boleh kosong');
+                return false;
+            }
+        }
+        return true;
     }
 }
