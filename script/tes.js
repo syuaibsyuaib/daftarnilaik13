d5dd1ad696c7b6d1ea71a122df3f19ebbf37299e
syuaibsyuaib <syuaibsyuaib@gmail.com>
2019-11-29 23:39:43 +0800

    perbaikan 3

--- script/tes.js
+++ script/tes.js
@@ -1,152 +1,163 @@
 const kdpkn = {
-    1.1: "1.1 Meyakini terdapat hubungan simbol pada lambang Negara dengan sila-sila Pancasila.",
-    2.1: "2.1 Menunjukkan sikap positif terhadap hubungan simbol pada lambang Negara dengan sila-sila Pancasila.",
-    3.1: "3.1 Memahami hubungan simbol pada lambang Negara dengan sila-sila Pancasila.",
-    4.1: "4.1 Menceritakan hubungan simbol pada lambang Negara dengan sila-sila Pancasila."
+    1.1: "Meyakini terdapat hubungan simbol pada lambang Negara dengan sila-sila Pancasila.",
+    2.1: "Menunjukkan sikap positif terhadap hubungan simbol pada lambang Negara dengan sila-sila Pancasila.",
+    3.1: "Memahami hubungan simbol pada lambang Negara dengan sila-sila Pancasila.",
+    4.1: "Menceritakan hubungan simbol pada lambang Negara dengan sila-sila Pancasila."
 },
     kdbind = {
-        3.1: "3.1 Memahami ungkapan, ajakan, perintah, penolakan yang terdapat dalam teks cerita atau lagu yang menggambarkan sikap hidup rukun.",
-        4.1: "4.1 Menirukan ungkapan, ajakan, perintah, penolakan dalam cerita atau lagu anak-anak dengan bahasa yang santun."
+        3.1: "Memahami ungkapan, ajakan, perintah, penolakan yang terdapat dalam teks cerita atau lagu yang menggambarkan sikap hidup rukun.",
+        4.1: "Menirukan ungkapan, ajakan, perintah, penolakan dalam cerita atau lagu anak-anak dengan bahasa yang santun."
     },
     kdmm = {
-        3.1: "3.1 Menjelaskan makna bilangan cacah dan menentukan lambangnya berdasarkan nilai tempat dengan menggunakan model konkret serta cara membacanya.",
-        4.1: "4.1 Membaca dan menyajikan bilangan cacah dan lambangnya berdasarkan nilai tempat dengan menggunakan model konkret",
-        3.2: "3.2 Membandingkan dua bilangan cacah.",
-        4.2: "4.2 Mengurutkan bilangan-bilangan dari bilangan terkecil ke bilangan terbesar atau sebaliknya.",
-        3.3: "3.3 Menjelaskan dan melakukan penjumlahan dan pengurangan bilangan yang melibatkan bilangan cacah sampai dengan 999 dalam kehidupan sehari-hari serta mengaitkan penjumlahan dan pengurangan.",
-        4.3: "4.3 Menyelesaikan masalah penjumlahan dan pengurangan bilangan yang melibatkan bilangan cacah sampai dengan 999 dalam kehidupan sehari-hari serta mengaitkan penjumlahan dan pengurangan."
+        3.1: "Menjelaskan makna bilangan cacah dan menentukan lambangnya berdasarkan nilai tempat dengan menggunakan model konkret serta cara membacanya.",
+        4.1: "Membaca dan menyajikan bilangan cacah dan lambangnya berdasarkan nilai tempat dengan menggunakan model konkret",
+        3.2: "Membandingkan dua bilangan cacah.",
+        4.2: "Mengurutkan bilangan-bilangan dari bilangan terkecil ke bilangan terbesar atau sebaliknya.",
+        3.3: "Menjelaskan dan melakukan penjumlahan dan pengurangan bilangan yang melibatkan bilangan cacah sampai dengan 999 dalam kehidupan sehari-hari serta mengaitkan penjumlahan dan pengurangan.",
+        4.3: "Menyelesaikan masalah penjumlahan dan pengurangan bilangan yang melibatkan bilangan cacah sampai dengan 999 dalam kehidupan sehari-hari serta mengaitkan penjumlahan dan pengurangan."
     },
     kdsbdp = {
-        3.2: "3.2 Mengenal pola irama sederhana melalui lagu anak-anak.",
-        4.2: "4.2 Menampilkan pola irama sederhana melalui lagu anak-anak.",
-        3.3: "3.3 Mengenal gerak keseharian dan alam dalam tari.",
-        4.3: "4.3 Meragakan gerak keseharian dan alam dalam tari.",
-        3.1: "3.1 Mengenal karya imajinatif dua dan tiga dimensi.",
-        4.1: "4.1 Membuat karya imajinatif dua dan tiga dimensi.",
-        3.4: "3.4 Mengenal pengolahan bahan alam dan buatan dalam berkarya.",
-        4.4: "4.4 Membuat hiasan dari bahan alam dan buatan."
+        3.2: "Mengenal pola irama sederhana melalui lagu anak-anak.",
+        4.2: "Menampilkan pola irama sederhana melalui lagu anak-anak.",
+        3.3: "Mengenal gerak keseharian dan alam dalam tari.",
+        4.3: "Meragakan gerak keseharian dan alam dalam tari.",
+        3.1: "Mengenal karya imajinatif dua dan tiga dimensi.",
+        4.1: "Membuat karya imajinatif dua dan tiga dimensi.",
+        3.4: "Mengenal pengolahan bahan alam dan buatan dalam berkarya.",
+        4.4: "Membuat hiasan dari bahan alam dan buatan."
     }
 const pemetaankd = function () {
     let kd = {
-        kls2smt1tm1sub1: {
-            mp: {
-                ppkn: {
-                    isikd: {
-                        1.1: kdpkn['1.1'],
-                        2.1: kdpkn['2.1'],
-                        3.1: kdpkn['3.1'],
-                        4.1: kdpkn['4.1']
+        kls2: {
+            smt1: {
+                tm1: {
+                    sub1: {
+                        ppkn: {
+                            isikd: {
+                                1.1: kdpkn['1.1'],
+                                2.1: kdpkn['2.1'],
+                                3.1: kdpkn['3.1'],
+                                4.1: kdpkn['4.1']
+                            },
+                            pb: [2, 5, 6]
+                        },
+                        bind: {
+                            isikd: {
+                                3.1: kdbind['3.1'],
+                                4.1: kdbind['4.1']
+                            },
+                            pb: [1, 2, 3, 4, 5, 6]
+                        },
+                        mm: {
+                            isikd: {
+                                3.1: kdmm['3.1'],
+                                4.1: kdmm['4.1']
+                            },
+                            pb: [1, 3, 4, 6]
+                        },
+                        sbdp: {
+                            isikd: {
+                                3.2: kdsbdp['3.2'],
+                                4.2: kdsbdp['4.2']
+                            },
+                            pd: [1, 3, 4]
+                        }
                     },
-                    pb: [2, 5, 6]
-                },
-                bind: {
-                    isikd: {
-                        3.1: kdbind['3.1'],
-                        4.1: kdbind['4.1']
+                    kls2smt1tm1sub2: function () {
+                        return {
+                            isi: kdpkn,
+                            kunci: Object.keys(kdpkn)
+                        };
                     },
-                    pb: [1, 2, 3, 4, 5, 6]
+                    kls2smt1tm1sub3: function () {
+                        return {
+                            isi: kdpkn,
+                            kunci: Object.keys(kdpkn)
+                        };
+                    },
+                    kls2smt1tm1sub4: function () {
+                        return {
+                            isi: kdpkn,
+                            kunci: Object.keys(kdpkn)
+                        };
+                    }
                 },
-                mm: {
-                    isikd: { 3.1: kdmm['3.1'], 4.1: kdmm['4.1'] },
-                    pb: [1, 3, 4, 6]
+                kls2smt1tm2sub1: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
                 },
-                sbdp: {
-                    isikd: { 3.2: kdsbdp['3.2'], 4.2: kdsbdp['4.2'] },
-                    pd: [1, 3, 4]
+                kls2smt1tm2sub2: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
+                },
+                kls2smt1tm2sub3: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
+                },
+                kls2smt1tm2sub4: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
+                },
+                kls2smt1tm3sub1: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
+                },
+                kls2smt1tm3sub2: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
+                },
+                kls2smt1tm3sub3: function () {
+                    return {
+                        isi: kdpkn,
+                        kunci: Object.keys(kdpkn)
+                    };
+                },
+                kls2smt1tm3sub4: function () {
+                    return {
+                        isi: kdbind,
+                        kunci: Object.keys(kdbind)
+                    };
+                },
+                kls2smt1tm4sub1: function () {
+                    return {
+                        isi: kdbind,
+                        kunci: Object.keys(kdbind)
+                    };
+                },
+                kls2smt1tm4sub2: function () {
+                    return {
+                        isi: kdbind,
+                        kunci: Object.keys(kdbind)
+                    };
+                },
+                kls2smt1tm4sub3: function () {
+                    return {
+                        isi: kdbind,
+                        kunci: Object.keys(kdbind)
+                    };
+                },
+                kls2smt1tm4sub4: function () {
+                    return {
+                        isi: kdbind,
+                        kunci: Object.keys(kdbind)
+                    };
                 }
             }
 
-        },
-        kls2smt1tm1sub2: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm1sub3: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm1sub4: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm2sub1: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm2sub2: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm2sub3: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm2sub4: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm3sub1: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm3sub2: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm3sub3: function () {
-            return {
-                isi: kdpkn,
-                kunci: Object.keys(kdpkn)
-            };
-        },
-        kls2smt1tm3sub4: function () {
-            return {
-                isi: kdbind,
-                kunci: Object.keys(kdbind)
-            };
-        },
-        kls2smt1tm4sub1: function () {
-            return {
-                isi: kdbind,
-                kunci: Object.keys(kdbind)
-            };
-        },
-        kls2smt1tm4sub2: function () {
-            return {
-                isi: kdbind,
-                kunci: Object.keys(kdbind)
-            };
-        },
-        kls2smt1tm4sub3: function () {
-            return {
-                isi: kdbind,
-                kunci: Object.keys(kdbind)
-            };
-        },
-        kls2smt1tm4sub4: function () {
-            return {
-                isi: kdbind,
-                kunci: Object.keys(kdbind)
-            };
         }
     }
+
     return kd;
 }
